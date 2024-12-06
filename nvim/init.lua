0151acc9fac812b85c6cdb1fa98eb5d0c93d62ca
kanetatsu <tatsuya_kaneda@kogasoftware.com>
2024-12-06 09:42:34 +0900

    キーマップ変更

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,8 +4,8 @@ require('lazy-nvim')
 vim.api.nvim_set_keymap('i', 'jj', '<ESC>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-h>', '<C-w><>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>>>', { noremap = true })
-vim.api.nvim_set_keymap('n', 'gp', ':bprev<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', 'gn', ':bnext<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-j>', ':bprev<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-k>', ':bnext<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<ESC><ESC>', ':nohl<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-[>', ':<C-u>tab stj <C-R>=expand("<cword>")<CR><CR>', { noremap = true })
 
@@ -13,6 +13,9 @@ vim.api.nvim_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', { noremap 
 vim.api.nvim_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', 'gf', '<cmd>lua vim.lsp.buf.format()<CR>', { noremap = true })
+
+vim.api.nvim_set_keymap('n', 'yy', '"+yy', { noremap = true })
+vim.api.nvim_set_keymap('v', 'y', '"+y', { noremap = true })
 
 -- set --
 vim.o.wildmenu = true
