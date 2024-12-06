1514da415307a36f317e974fee18e1d7cd0ad840
kanetatsu <tatsuya_kaneda@kogasoftware.com>
2024-12-06 09:43:10 +0900

    Win 環境の ToggleTerm デフォルトシェルを GitBash にする

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -2,6 +2,11 @@ return {
   {
     'akinsho/toggleterm.nvim',
     config = function()
+      if vim.loop.os_uname().sysname == 'Windows_NT' then
+        vim.cmd [[let &shell = '"C:/Program Files/Git/bin/bash.exe"']]
+        vim.cmd [[let &shellcmdflag = '-s']]
+      end
+
       require('toggleterm').setup({
         direction = 'float',
         open_mapping = [[<c-\>]],
